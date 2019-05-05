3a8f375dcca3da3013f1987d150f5806acf58b19
Robert Wolff <ro.wolff@gmx.net>
2019-05-05 14:02:00 +0200

    class methods

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import { generateRandomId } from "./utils";
 
 class App {
   static id: string = "app";
+  onInit(el: HTMLElement | null): void {
+    setInterval(function() {
+      if (el) {
+        el.innerHTML = generateRandomId({ symbol: "#", length: 7 });
+      }
+    }, 1000);
+  }
 }
 
 function main(ComponentClass: any): void {
-  const app = document.getElementById(ComponentClass.id);
-  setInterval(function() {
-    if (app) {
-      app.innerHTML = generateRandomId({ symbol: "#", length: 7 });
-    }
-  }, 1000);
+  const el = document.getElementById(ComponentClass.id);
+  const cmp = new ComponentClass();
+  cmp.onInit(el);
 }
 
 main(App);
