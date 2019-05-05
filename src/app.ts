56999c27bee2422c376a63a4d8e2d0af1dba0ae2
Robert Wolff <ro.wolff@gmx.net>
2019-05-05 13:39:28 +0200

    remove unused example functions

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,5 @@
 import { generateRandomId } from "./utils";
 
-function identity<T>(arg: T): T {
-  return arg;
-}
-
-identity<string>("James");
-
-function userAlert(): void {
-  alert("Hello!");
-}
-
-function someError(): never {
-  throw new Error("Failed!");
-}
-
 function main() {
   const app = document.getElementById("app");
   setInterval(function() {
